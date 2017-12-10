bc73b67e6c4d6e04b7f87470a5e924124b9dcdb8
lipiner <ylipiner@gmail.com>
2017-12-10 13:56:47 +0200

    CompilationEngine

--- JackCompiler.py
+++ JackCompiler.py
@@ -10,7 +10,7 @@ from CompilationEngine import CompilationEngine
 # constants #
 #############
 PATH_POS = 1  # the arguments position for the file path
-VM_SUFFIX = ".xml"
+VM_SUFFIX = ".vm"
 JACK_SUFFIX = ".jack"
 WRITING_MODE = "w"
 FILE_NAME_POSITION = -1
